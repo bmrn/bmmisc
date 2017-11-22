778b0ddf71250a56b92c509d7b9664de957464d9
Ben McElduff <ausben@gmail.com>
2017-11-22 16:55:04 +1100

    add lmprint function

--- R/one_liners.R
+++ R/one_liners.R
@@ -1,3 +1,21 @@
+#' Print a loading matrix
+#'
+#' Prints a matrix with all entries below a given cut off removed. Useful
+#' to print loading matrices in factor analysis.
+#' @param loadings numerical matrix of variable loadings e.g.
+#' from princomp()$loadings
+#' @param cutoff numbers <= to cutoff will not be printed
+#' @param digits number of significant digits to print
+#'
+#' @keywords pca fa factanal princomp
+#' @export
+#' @examples
+#' set.seed(10)
+#' lmprint(matrix(runif(9), 3, 3))
+lmprint <- function(loadings, cutoff = 0.35, digits = 3) {
+  print(ifelse(abs(loadings) > cutoff, loadings, NA),
+        digits = digits, na.print = "")
+}
 #' is wholenumer
 #'
 #' Tests if numeric object is a whole number within a given tolerence.
